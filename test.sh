2df625d29b8fced5102220278ddece3b5ddf445a
高京 <jing.gao.b@17zuoye.com>
2019-09-03 19:37:15 +0800

    test jenkins

--- test.sh
+++ test.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 branch=$GIT_BRANCH
 working_copy=$WORKSPACE
-echo $branch
+
+stage="test"
+if("$branch"="origin/master")
+    stage="test"
+else if("$branch"="origin/staging")
+    stage="stage"
+else if("$branch")="origin/release")
+    stage="release"
+fi
+
+echo $stage
 echo "test.sh hehe $working_copy"
