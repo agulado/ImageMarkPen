a34e4e33f350db915fb496235b453319d0a9be44
高京 <jing.gao.b@17zuoye.com>
2019-09-04 11:16:28 +0800

    test jenkins

--- test.sh
+++ test.sh
@@ -25,6 +25,6 @@ echo
 function dealPath(){
     echo '26: $1';
 }
-git diff $GIT_COMMIT $GIT_PREVIOUS_COMMIT --name-only $1 | xargs -n1 dealPath {}
+git diff $GIT_COMMIT $GIT_PREVIOUS_COMMIT --name-only $1 | xargs -n1 -I {} -p {} dealPath
 
 echo
