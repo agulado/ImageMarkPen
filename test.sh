bfb7513e274605c33ee3f39a265cbfd00b048f3f
高京 <jing.gao.b@17zuoye.com>
2019-09-04 17:34:51 +0800

    test jenkins

--- test.sh
+++ test.sh
@@ -80,7 +80,7 @@ function dealPath(){
         _arr=(${path//\// })
 
         echo "82 _arr.length=" ${#_arr[@]}
-        `echo "83:" \$directory_level_$file_type`
+        `echo \$directory_level_$file_type`
         continue;
         
         # case $file_type in
